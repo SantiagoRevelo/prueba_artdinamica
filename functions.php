e4cc5a8dbc7dd8ac533d1a166fd83f673c7d5392
SantiagoRevelo <sreveloc@gmail.com>
2019-10-30 13:13:02 +0100

    prueba PHP

---
+++ functions.php
@@ -0,0 +1,157 @@
+<?php
+    $user   = "db_user";
+    $pass   = "123456789";
+    $server = "localhost";
+    $bbdd   = "artdinamica";
+       
+    $connection = mysqli_connect( $server, $user, $pass );
+    $connection->query("SET NAMES 'utf8'");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    function get_employees() {
+        $q = '  
+                SELECT  t1.id, t1.name, t1.lastname, t1.address, t1.birth_date, t1.dni, 
+                        t2.start_date, t2.finish_date, 
+                        t3.name as "location", 
+                        t4.name as "charge", t4.job_hours, t4.entrance_time, t4.exit_time
+                FROM employees t1, contracts t2 , workplaces t3, roles t4
+                WHERE t1.contract_id = t2.id AND t2.workplace_id = t3.id AND t2.role_id = t4.id;
+        ';
+        $result = get_data_from_ddbb( $q );
+        $employees = [];
+        while ( $row = $result->fetch_object() ){
+            $employees[] = $row;
+        }
+        return $employees;
+    }
+
+    function get_user_reports( $employee_id ) {
+        $q = 'SELECT * FROM reports where employee_id = ' .$employee_id;
+        $result = get_data_from_ddbb( $q );
+        $reports= [];
+        while ( $row = $result->fetch_object() ){
+            $reports[] = $row;
+        }
+        return $reports;
+    }
+    function get_data_from_ddbb($query) {
+        global $connection, $bbdd;
+        $db = mysqli_select_db( $connection, $bbdd ) or die( 'No se puede conectar con la bbdd: ' .$bbdd );
+        $result = mysqli_query( $connection, $query ) or die( 'No se ha podido realizar la consulta: ' .$query);
+
+        return $result;
+    }
+
+    function check_contract_hours() {
+        // Get employees
+        $employees = get_employees();
+        
+        $employees_schedule = [];
+        
+        // Iterate through the employees
+        foreach ($employees as $emp) {
+            $employee_reports = get_user_reports($emp->id);
+
+            $tmp_report = [];
+
+            $total_work_hours = 0;
+            $total_work_days = 0;
+            $average_hours_per_day = 0;
+            
+            $tmp_days_ok_count = 0;
+            $tmp_days_ok = [];
+
+            $tmp_days_overtime_count = 0;
+            $tmp_days_overtime = [];
+            
+            $tmp_days_incomplete_count = 0;
+            $tmp_days_incomplete = [];
+
+            $employee_deviation = 0;
+
+            // Iterate through the employees's reports
+            foreach ($employee_reports as $report) {
+                // check for overtimed work days
+                if ( $report->hours_worked > $emp->job_hours ) {
+                    $tmp_days_overtime_count++;
+                    $tmp_days_overtime[] = Array(
+                        'date' => $report->date,
+                        'hours' => $report->hours_worked - $emp->job_hours
+                    );
+                }
+                // check for incomplete work days
+                if ( $report->hours_worked > $emp->job_hours ) {
+                    $tmp_days_incomplete_count++;
+                    $tmp_days_incomplete[] = Array(
+                        'date' => $report->date,
+                        'hours' => $report->hours_worked - $emp->job_hours
+                    );
+                }
+                // check for complete work days
+                if ( $report->hours_worked == $emp->job_hours ) {
+                    $tmp_days_ok_count++;
+                    $tmp_days_ok[] = Array(
+                        'date' => $report->date,
+                        'hours' => $report->hours_worked - $emp->job_hours
+                    );
+                }
+                $total_work_days += 1;
+                $total_work_hours += $report->hours_worked;
+            }
+            
+            if ($total_work_days > 0) {
+                /** Deviation **/
+                // D = SQRT( (∑|x-π|)^2 / N)
+                // 1. Calc. Average work_hours (π)            
+                $average_hours_per_day = $total_work_hours / $total_work_days;
+                $deltaHours = 0;
+                // 2. For each hours data
+                foreach ($employee_reports as $report) {
+                    // 3. Sum power 2 distance to average hours ∑(x-π)^2
+                    //$deltaHours += pow($report->hours_worked - $average_hours_per_day, 2);
+                    $deltaHours += pow($report->hours_worked - $emp->job_hours, 2);
+                }
+                // Paso 4: divide by data count. (N)
+                $dev_sqrt = $deltaHours / $total_work_days;
+                // Paso 5: calc square root.
+                $employee_deviation = sqrt($dev_sqrt);
+            }
+
+            // Set the time report of an employee
+            $tmp_report = Array(
+                'employee_id' => $emp->id,
+                'name' => $emp->name,
+                'lastname' => $emp->lastname,
+                'address' => $emp->address,
+                'birth_date' => $emp->birth_date,
+                'dni' => $emp->birth_date,
+                'location' => $emp->location,
+                'charge' => $emp->charge,
+                'start_date' => $emp->start_date,
+                'finish_date'=> $emp->finish_date,
+                'entrance_time' => $emp->entrance_time,
+                'exit_time' => $emp->exit_time,
+                'total_work_days' => (int)$total_work_days,
+                'average_hours_by_day' => (float)$average_hours_per_day,
+                'job_hours' => (float)$emp->job_hours,
+                'deviation' =>  (float)$employee_deviation,
+                'time_report' => Array(                
+                    'days_ok' => (int)$tmp_days_ok_count,
+                    'days_ok_dates' => $tmp_days_ok,
+                    'days_overtime' => (int)$tmp_days_overtime_count,
+                    'days_overtime_dates' => $tmp_days_overtime,
+                    'days_incomplete' => (int)$tmp_days_overtime_count,
+                    'days_incomplete_dates' => $tmp_days_incomplete,
+                )
+            );
+            $employees_schedule[] = $tmp_report;
+        }
+        // Dump the full timeReport
+        //var_dump($employees_schedule);
+        //show_alerts($employees_schedule);
+
+        return $employees_schedule;
+    }
+?>
---
+++ index.php
@@ -0,0 +1,159 @@
+<?php
+    // import functions
+    require 'functions.php';
+    // get employees data
+    $schedule = check_contract_hours();
+    // set data type styles and info
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Employees report</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+
+    <body>
+        <div class="container">     
+                <?php
+                    foreach ($schedule as $emp) {
+                        if ( $emp['job_hours'] > $emp['average_hours_by_day'] ) {
+                            $alert_type = 'danger';
+                            $desc = 'no está cumpliendo con su contrato';
+                        } else if ( $emp['job_hours'] < $emp['average_hours_by_day'] ) {
+                            $alert_type = 'warning';
+                            $desc = 'está cumpliendo mas que su contrato';
+                        } else if ( $emp['job_hours'] === $emp['average_hours_by_day'] ) {
+                            $alert_type = 'success';
+                            $desc = 'cumple con su contrato';
+                        }
+                ?>
+                        <div class="alert alert-<?php echo $alert_type; ?>" role="alert">
+                            <p>El empleado <strong><?php echo $emp['name'] .' ' .$emp['lastname']; ?></strong> <?php echo $desc; ?></p>
+                            <hr>
+                            <div class="py-4">Su desviación es: <?php echo number_format($emp['deviation'],2); ?> 
+                                <a class="btn btn-<?php echo $alert_type; ?> btn-sm float-right" 
+                                    data-toggle="collapse" 
+                                    href="#employeeInfo<?php echo $emp['employee_id']; ?>" 
+                                    role="button" 
+                                    aria-expanded="false" 
+                                    aria-controls="#employeeInfo<?php echo $emp['employee_id']; ?>">
+                                    Más información
+                                </a>
+                            </div>
+                            <div class="collapse multi-collapse" id="employeeInfo<?php echo $emp['employee_id']; ?>">
+                                <div class="info-panel">
+                                    <div class="dates">
+                                        <div class="start">
+                                            <strong>Inicio del contrato</strong> <?php echo $emp['start_date'] ?>
+                                            <span></span>
+                                        </div>
+                                        <div class="ends">
+                                            <strong>Fin del contrato</strong> <?php echo isset($emp['finish_date']) ? $emp['finish_date'] : '---' ?>
+                                            <span></span>
+                                        </div>
+                                    </div>
+                                    <div class="row info-group">
+                                        <h5 class="w-100">Datos personales</h5>
+                                        <div class="col col-12 col-sm-12 col-md-3">
+                                            <strong>Employee ID</strong> <?php echo $emp['employee_id'] ?>
+                                        </div>
+                                        <div class="col col-12 col-sm-12 col-lg-5">
+                                            <strong>Nombre</strong> <?php echo $emp['name'] .' ' .$emp['lastname']?>
+                                        </div>
+                                        <div class="col col-6 col-lg-2">
+                                            <strong>Fecha nacimiento</strong> <?php echo $emp['birth_date'] ?>
+                                        </div>
+                                        <div class="col col-6 col-lg-2">
+                                            <strong>DNI</strong> <?php echo $emp['dni'] ?>
+                                        </div>
+                                        <div class="w-100"></div>
+                                        <div class="col col-12">
+                                            <strong>Dirección</strong> <?php echo $emp['address'] ?>
+                                        </div>                                                
+                                    </div>
+                                    
+                                    <div class="row info-group">
+                                        <h5 class="w-100">Datos de contrato</h5>
+                                        <div class="col col-4 col-sm-auto">
+                                            <strong>Cargo</strong> <?php echo $emp['charge'] ?>
+                                        </div>
+                                        <div class="col col-4 col-sm-auto">
+                                            <strong>Horas de trabajo</strong> <?php echo $emp['job_hours'] ?>
+                                        </div>
+                                        <div class="col col-4 col-sm-1auto">
+                                            <strong>Localización</strong> <?php echo $emp['location'] ?>
+                                        </div>
+                                        <div class="col col-4 col-sm-auto">
+                                            <strong>Hora de entrada</strong> <?php echo $emp['entrance_time'] ?>
+                                        </div>
+                                        <div class="col col-4 col-sm-auto">
+                                            <strong>Hora de salida</strong> <?php echo $emp['exit_time'] ?>
+                                        </div>                                                
+                                    </div>
+                                    <div class="row info-group">
+                                        <h5 class="w-100">Datos de trabajo</h5>
+                                        <div class="col col-6 col-sm-auto">
+                                            <strong>Días trabajados</strong> <?php echo $emp['total_work_days'] ?>
+                                        </div>
+                                        <div class="col col-6 col-sm-auto">
+                                            <strong>Media de horas trabajadas</strong> <?php echo $emp['average_hours_by_day'] ?>
+                                        </div>                         
+                                    </div>
+                                    <div class="row info-group">
+                                        
+                                        <div class="col col-12 col-lg-4">
+                                            <strong>Días cumplidos</strong> <?php echo $emp['time_report']['days_ok'] ?>
+                                            <div>
+                                                <?php
+                                                    foreach ($emp['time_report']['days_ok_dates'] as $val) {
+                                                ?>
+                                                        <span class="badge badge-pill badge-success"><?php echo $val['date']; ?></span>
+                                                <?php
+                                                    }
+                                                ?>
+                                            </div>
+                                        </div>  
+                                        
+                                        <div class="col col-12 col-lg-4">
+                                            <strong>Días incumplidos</strong> <?php echo $emp['time_report']['days_incomplete'] ?>
+                                            <div>
+                                                <?php
+                                                    foreach ($emp['time_report']['days_incomplete_dates'] as $val) {
+                                                ?>
+                                                        <span class="badge badge-pill badge-danger"><?php echo $val['date']; ?></span>
+                                                <?php
+                                                    }
+                                                ?>
+                                            </div>
+                                        </div>  
+
+                                        <div class="col col-12 col-lg-4">
+                                            <strong>Días Horas extra</strong> <?php echo $emp['time_report']['days_overtime'] ?>
+                                            <div>
+                                                <?php
+                                                    foreach ($emp['time_report']['days_overtime_dates'] as $val) {
+                                                ?>
+                                                        <span class="badge badge-pill badge-warning"><?php echo $val['date']; ?></span>
+                                                <?php
+                                                    }
+                                                ?>
+                                            </div>
+                                        </div>  
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                <?php
+                    }
+                ?>
+        </div>
+    </body>
+</html>
